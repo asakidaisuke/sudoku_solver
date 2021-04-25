4b1507d258820b72b42aa6622a8015953a7cc440
asakidaisuke <mandm6523@gmail.com>
2021-04-25 11:28:34 +0900

    add interface

--- SMT_translator/SMT_translator.py
+++ SMT_translator/SMT_translator.py
@@ -1,3 +1,5 @@
+from sudoku import *
+
 class SMT_translator:
     def __init__(self, file_name :str = None,  
                  row :int = 9, column :int = 9):
@@ -9,7 +11,7 @@ class SMT_translator:
             raise ValueError("The number of row and column must be 9 or 16.")
             
         self.cage_input_list = []
-        self.output_file = ""
+        self.output_file = "myfile.smt2"
         
         self.row = row
         self.column = column
@@ -32,71 +34,55 @@ class SMT_translator:
     def __declaration(self) -> None:
         for r in range(1, self.row + 1):
             for c in range(1, self.column + 1):
-                state = "(declare-fun x" + str(r) + str(c) + " () Int) \n"
-                self.str_declaration_list.append(state)
+                self.str_declaration_list.append(show_declare_int(var(r,c)) + "\n")
                 
     def __max_min_constrain(self) -> None:
         for r in range(1, self.row + 1):
             for c in range(1, self.column + 1):
-                state = "(<= 1 x" + str(r) + str(c) + ") (<= x" + str(r) + str(c) + " 9) \n"
-                self.str_max_min_constrain_list.append(self.tab_space + state)
+                first_state = show(morethan(1, var(r,c)))
+                decond_state = show(lessthan(9, var(r,c)))
+                state = first_state + " " +  decond_state
+                self.str_max_min_constrain_list.append(self.tab_space + state + "\n")
         
     def __distinct_constrain_row(self) -> None:
-        prefix = "(distinct "
-        suffix = ")\n"
         for r in range(1, self.row + 1):
-            state = ""
+            items = []
             for c in range(1, self.column + 1):
-                state += " x" + str(r) + str(c)
+                items.append(var(r, c))
             self.str_distinct_constrain_row_list.append(
-                self.tab_space + prefix + state + suffix)
+                self.tab_space + show_distinct(items) + "\n")
         
     def __distinct_constrain_column(self) -> None:
-        prefix = "(distinct "
-        suffix = ")\n"
         for c in range(1, self.column + 1):
-            state = ""
+            items = []
             for r in range(1, self.row + 1):
-                state += " x" + str(r) + str(c)
+                items.append(var(r, c))
             self.str_distinct_constrain_column_list.append(
-                self.tab_space + prefix + state + suffix)
+                self.tab_space + show_distinct(items) + "\n")
             
     def __distinct_constrain_nonet(self) -> None:
-        prefix = "(distinct "
-        suffix = ")\n"
         for c_mesh in range(self.mesh):
             for r_mesh in range(self.mesh):
-                state = ""
+                items = []
                 for c_offset in range(1, self.mesh + 1):
                     for r_offset in range(1, self.mesh + 1):
                         r_base = r_mesh * self.mesh
                         c_base = c_mesh * self.mesh
-                        state += " x" + str(r_base + r_offset) + str(c_base + c_offset)
+                        items.append(var(r_base + r_offset, c_base + c_offset))
                 self.str_distinct_constrain_nonet_list.append(
-                    self.tab_space + prefix + state + suffix)
+                    self.tab_space + show_distinct(items) + "\n")
     
-
     def __cage_constrain(self) -> None:
-        pre_suffix = ") "
-        suffix = ")\n"
         for cage_input in self.cage_input_list:
-            state_middle = self.tab_space + "(= (+"
-            state_post = ""
-            for i in range(len(cage_input)):
-                if i == 0:
-                    state_post = pre_suffix + str(cage_input[i]) + suffix
-                else:
-                    state_middle += " " + "x" + str(cage_input[i])
-            self.str_cage_constrain_list.append(state_middle + state_post)
+            self.str_cage_constrain_list.append(
+                self.tab_space + show(cage_input) + "\n")
     
     def __show_result(self) -> None:
-        prefix = "(get-value  ("
-        suffix = "))\n"
         for r in range(1, self.row + 1):
-            state = ""
+            get_list = []
             for c in range(1, self.column + 1):
-                state += " x" + str(r) + str(c)
-            self.show_result_list.append(prefix + state + suffix)
+                get_list.append(var(r, c))
+            self.show_result_list.append(show_get_value(get_list) + "\n")
     
     def __write_file(self):
         file1 = open(self.output_file,"w")
@@ -127,4 +113,3 @@ class SMT_translator:
         self.__cage_constrain()
         self.__show_result()
         self.__write_file()
-        
--- SMT_translator/killer.py
+++ SMT_translator/killer.py
@@ -1,25 +1,20 @@
 import sys
 from collections import defaultdict
+import subprocess
 
 from SMT_translator import SMT_translator
+from sudoku import create_input_list, read_model
 
 
-def create_input_list(input_list :list) -> list:
-    """
-    read input file and make input list
-    """
-    input_item_list = []
-    for input_item in input_list:
-        item_list = []
-        input_item_splited = input_item.split(' ')
-        for i in range(len(input_item_splited)):
-            if "\n" in input_item_splited[i]:
-                input_item_splited[i] = input_item_splited[i].replace("\n", "")
-            if input_item_splited[i] == "":
-                continue
-            item_list.append(input_item_splited[i])
-        input_item_list.append(item_list)
-    return input_item_list
+def smt(cage_input :list, output_file_name :str = 'b.smt2') -> list:
+    smt_translator = SMT_translator()
+    smt_translator.set_output_file(output_file_name)
+    smt_translator.set_cageinput(input_item_list)
+    smt_translator.translate()
+    
+    z3_output = subprocess.check_output(['z3 ' + output_file_name], shell=True)
+    z3_output_str = z3_output.decode("utf-8")
+    return read_model(z3_output_str)
 
 if __name__ == "__main__":
     output_file_name = sys.argv[2]
@@ -28,8 +23,4 @@ if __name__ == "__main__":
         input_list = my_file.readlines()
     input_item_list = create_input_list(input_list)
     
-    smt_translator = SMT_translator()
-    smt_translator.set_output_file(output_file_name)
-    smt_translator.set_cageinput(input_item_list)
-    smt_translator.translate()
-    print("")
+    print(smt(input_item_list))
