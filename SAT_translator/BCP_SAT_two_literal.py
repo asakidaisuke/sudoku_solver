56de4408c1e891b63a0b081daec97f18f9055de7
askaidaisuke <mandm6523@gmail.com>
2021-05-19 23:16:10 +0900

    modify implementation

--- SAT_translator/BCP_SAT_two_literal.py
+++ SAT_translator/BCP_SAT_two_literal.py
@@ -1,18 +1,19 @@
-import sys
 from collections import defaultdict
+import time
+import sys
 
 
-def lister(nums :list) -> list:
-    a = [[],[]]
+def lister(nums: list) -> list:
+    a = [[], []]
     for num in nums:
         if num > 0:
             a[0].append(num - 1)
         else:
-            a[1].append(-num -1)
+            a[1].append(-num - 1)
     return a
 
 def make_two_watched_table(
-            CNF, watch_tabl, all_table
+                CNF, watch_table
         ):
     for i in range(len(CNF)):
         vars = CNF[i][0] + CNF[i][1]
@@ -20,74 +21,106 @@ def make_two_watched_table(
             watch_table[i] = vars[0:2]
         else:
             watch_table[i].append(vars[0])
-        all_table[i] = vars
 
-
-def find_implication_from_table(index: int, current_list: list, first_look=False):
+def find_unit_clause(index: int, current_list: list, unit_clause: list):
+    """
+    新しく割り当てられた変数にtriggerされて、新しくunit状態となった
+    clauseを列挙して返す。その過程でコンフリクトを見つけた場合はNoneを返す。
+    """
+    # 新しい割り当てがない場合はliteralが1個しかないclauseをunit状態のclauseとして返す。
     if index is None:
-        return single_literals
-    implied_clause = [key for key, value in watch_table.items() if index in value]
-    implied_clause = find_true_implication(implied_clause, index, current_list)
-    if first_look:
-        for i in range(index):
-            implied_clause += find_true_implication(
-                [key for key, value in watch_table.items() if i in value],
-                i,
-                current_list
-            )
-        implied_clause = list(set(implied_clause))
-        implied_clause += single_literals
-    return implied_clause
+        for literal in (single_literals):
+            unit_clause.append(literal)
+        return True
+    # 新しく割り当てられた変数をwatchしているclauseを列挙する。
+    including_clause = [key for key, value in watch_table.items() if index in value]
+    # 新しく割り当てられた変数をwatchしているclauseからunit状態のものを抽出。
+    # コンフリクトがあった場合はNoneを返す。
+    noConflict = list_is_unit_clause(including_clause, index, current_list, unit_clause)
+    return noConflict
 
-
-def find_true_implication(
-        implied_clause: list, index: int, current_list: list
-) -> list:
-    truely_implied_clause = []
-    for clause in implied_clause:
-        # satになった場合は除外
+def list_is_unit_clause(
+                including_clause: list, index: int, current_list: list, unit_clause: list
+        ) -> list:
+    """
+    watching literalに新しくassingした片酢が入ってた物の中から、
+    コンフリクトと新しいunit状態のclauseを探し出す。
+    """
+    for clause in including_clause:
+        if clause in unit_clause:
+            continue
+        # 新しいassignでsatになった場合は除外
         if (current_list[index] and index in CNF[clause][0]) or \
                 (not current_list[index] and index in CNF[clause][1]):
             continue
-        # implication状態になったclauseのwatch listを取得
+        # watch listを取得
         watch_literal = watch_table[clause]
-        # implication状態になったclauseのwatch list以外をリスト化
-        not_watch_literal = [literal for literal in all_table[clause] if literal not in watch_literal]
-        # unassigned状態の変数があればそれを監視下に置換する
-        not_assinged = [literal for literal in not_watch_literal if current_list[literal] is None]
-        if len(not_assinged) > 0:
-            watch_table[clause][watch_table[clause].index(index)] = not_assinged[0]
-        elif len(watch_table[clause]) == 1:
-            truely_implied_clause.append(clause)
+        other_literal = list(set(watch_literal) - set([index]))[0]
+        # unit状態になったclauseのwatching literal以外のliteralをリスト化
+        not_watch_literal = list(set(CNF[clause][0] + CNF[clause][1]) - set(watch_literal))
+        if len(not_watch_literal) > 0:
+            # not Falseの変数を探す、あればそれを監視下に置換する
+            assignable = [
+                literal for literal in not_watch_literal
+                if current_list[literal] is None or
+                   (current_list[literal] and literal in CNF[clause][0]) or
+                   (not current_list[literal] and literal in CNF[clause][1])
+                    ]
         else:
-            truely_implied_clause.append(clause)
-    return truely_implied_clause
+            assignable = []
 
+        if len(assignable) > 0:
+            # not Falseの変数があればそれを監視下に置き換え
+            watch_table[clause][watch_table[clause].index(index)] = assignable[0]
+        else:
+            # not Falseの変数がなくて片方の監視下literalがNoneならそれはunit状態
+            if current_list[other_literal] is None:
+                unit_clause.append(
+                    (
+                        clause,
+                        other_literal
+                    )
+                )
+                # もう一方のliteralが1の場合satなので飛ばす
+            elif (current_list[other_literal] and other_literal in CNF[clause][0]) or \
+                    (not current_list[other_literal] and other_literal in CNF[clause][1]):
+                continue
+            else:
+                # もう一方のliteralも0の場合conflict
+                return None
+    return True
 
 def two_watched(current_list: list, CNF: list, index: int) -> bool:
     X = current_list.copy()
-    new_implied_clause = find_implication_from_table(index, X, first_look=True)
-    while len(new_implied_clause) > 0:
-        implied_clause = new_implied_clause
-        new_implied_clause = []
-        for clause in implied_clause:
-
-            P, N = CNF[clause]
-            I = [i for i in P if X[i] != False] + \
-                [i for i in N if X[i] != True]
-
-            if I == []:
-                return False
-            i = I.pop()
-            if I == [] and X[i] == None:
-                X[i] = i in P
-                new_implied_clause += find_implication_from_table(i, X)
-                new_implied_clause = list(set(new_implied_clause))
+    # まず最初に新しく割り当てられてた変数からtriggerされるunit clauseを探索する。
+    unit_clause = []
+    noConflict = find_unit_clause(index, X, unit_clause)
+    # コンフリクトがある場合はNoneになる。
+    if noConflict is None:
+        return False
+    # unit_clauseにはunit状態のclauseの番号と割り当て待ちの変数が入っている。
+    while len(unit_clause) > 0:
+        clause = unit_clause.pop()
+        # unit状態のclauseを取り出す。
+        P, N = CNF[clause[0]]
+        # 変数に割り当てを行う。
+        X[clause[1]] = clause[1] in P
+        # 新しい割り当てでtriggerされたunit状態のclauseがあるかを調べる。
+        noConflict = find_unit_clause(clause[1], X, unit_clause)
+        # conflictがあればNoneが返ってくる。
+        if noConflict is None:
+            return False
+        # # 新しくunit状態となったものを保持しておき次のiterationで割り当てていく。
+    if index is None:
+        # 一つだけのclauseで割り当てられる変数はその後変動がないので、
+        # 決定の割り付けとして最初の段階でXを更新する。
+        for i in range(len(X)):
+            current_list[i] = X[i]
     return True
 
 
 def search(X, CNF, index=None):
-    print(X)
+    # print(X)
     if not two_watched(X, CNF, index):
         return None
     if not None in X:
@@ -103,16 +136,18 @@ def search(X, CNF, index=None):
 
 
 def write_output(file_name: str, output: list) -> None:
-    output = [str(i+1) + ' ' if output[i] else str(-(i+1)) + ' ' for i in range(len(output))] + ['0']
-    with open(file_name,'w') as f:
+    output = [str(i + 1) + ' ' if output[i] else str(-(i + 1)) + ' ' for i in range(len(output))] + ['0']
+    with open(file_name, 'w') as f:
         f.writelines('SAT \n')
         f.writelines("".join(output))
     f.close()
 
-if __name__  == '__main__':
+
+if __name__ == '__main__':
+    # start = time.time()
     cnf_file = sys.argv[1]
     output_file = sys.argv[2]
-    
+
     with open(cnf_file) as f:
         input_list = f.readlines()
     f.close()
@@ -123,12 +158,11 @@ if __name__  == '__main__':
 
     watch_table = defaultdict(list)
     watch_table_reverse = defaultdict(list)
-    all_table = defaultdict(list)
-    make_two_watched_table(CNF, watch_table, all_table)
-#     make_two_watched_table_reverse(watch_table_reverse, watch_table)
-    single_literals = [key for key, value in watch_table.items() if len(value) == 1]
+    make_two_watched_table(CNF, watch_table)
+    single_literals = [(key, value) for key, value in watch_table.items() if len(value) == 1]
+    single_literals = [(clause[0], clause[1][0]) for clause in single_literals]
 
 
     output = search(none_list, CNF)
     write_output(output_file, output)
-   
+    # print(time.time() - start)
